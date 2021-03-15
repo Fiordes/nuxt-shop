c2403d1d55e1b4c1507295b2cb8022feffb6025f
Fiordes <48431306+Fiordes@users.noreply.github.com>
2021-03-15 21:32:41 +0200

    Starting

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+  mounted() {},
+  computed: {}
+};
+</script>
+<style lang="scss" scoped>
+.container {
+  @include media(wide) {
+    display: none;
+  }
+}
+</style>
